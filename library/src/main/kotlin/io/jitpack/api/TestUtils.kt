7d1c37107bc60bfa595fb023acc1985a33e92242
xuguangzhi <2560999091@qq.com>
2023-09-25 18:36:23 +0800

    second

--- library/src/main/kotlin/io/jitpack/api/TestUtils.kt
+++ library/src/main/kotlin/io/jitpack/api/TestUtils.kt
@@ -2,9 +2,12 @@ package io.jitpack.api
 
 object TestUtils {
 
+    val imageList = listOf<Int>(R.mipmap.a1,R.mipmap.a2,R.mipmap.a3,R.mipmap.a4,R.mipmap.a5)
+
     @JvmStatic
     fun testPrint(){
         println("Hello World!")
+        println(imageList.size.toString())
     }
 
 }
